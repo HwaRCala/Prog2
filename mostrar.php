1625f761a326caa80129359645c5991d84d88c89
HwaRCala <leo.lviola@gmail.com>
2023-05-01 16:30:11 -0300

    Ultimo

--- mostrar.php
+++ mostrar.php
@@ -2,7 +2,7 @@
 <body>
 	Hola <?php echo $_POST["nombre"]; ?> <br>
 	tu edad es : <?php echo $_POST["edad"]; ?><br>
-	tu Email es : <?php echo $_POST["user-email"]; ?><br>
+	tu Email es : <?php echo $_POST["useremail"]; ?><br>
 	tu domicilio es : <?php echo $_POST["direccion"]; ?><br>
 	tu localidad es : <?php echo $_POST["localidad"]; ?><br>
 </body>
