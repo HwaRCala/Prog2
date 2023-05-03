ac6a431ae2ea4ebe29dd7d9f03d4e31c6b4a5b78
HwaRCala <leo.lviola@gmail.com>
2023-05-03 13:51:55 -0300

    03052023

---
+++ Conexion/test_conn.php
@@ -0,0 +1,37 @@
+<?php
+require `db_conn.php`;
+?>
+
+<html lang="en">
+
+<head>
+    <title>mi pagina de conexion</title>
+</head>
+
+<body class="">
+
+    <div class="">
+        <div class="">
+            <div class=""><i class=""></i></div>
+                <div class="" data-postfix="" data-duration="1500" data-delay="0">
+                <h4>Departamento</h4><br>
+                <?php
+                    $query = "select * from departamento";
+
+                    //echo $query;
+                    $result = mysqli_query($con, $query);
+
+                    if (mysqli_affected_rows($con) != 0) {
+                        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                            echo $row[`nombre`] . `<br>`;
+                        }
+                    }else{
+                        echo `sin datos`;
+                    }
+                    ?>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
